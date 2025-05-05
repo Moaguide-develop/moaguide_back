3c24baa049198bc5be67bddfdf0a9eea33ed2148
LJH <qufwnsgh3@naver.com>
2025-05-06 03:56:18 +0900

    fix: 롴러에서 안쓰이느부분 수정

--- src/main/java/com/moaguide/oauth2/CustomClientRegistrationRepo.java
+++ src/main/java/com/moaguide/oauth2/CustomClientRegistrationRepo.java
@@ -2,10 +2,12 @@ package com.moaguide.oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
+@Profile({"blue","green"})
 public class CustomClientRegistrationRepo {
 
     private final SocialClientRegistration socialClientRegistration;
